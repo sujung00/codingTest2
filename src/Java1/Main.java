6352413c09c5597598420e22a27db782801f136a
sujung00 <rlatnwjdirin@naver.com>
2022-09-30 15:06:52 +0900

    Initial commit

---
+++ src/Java1/Main.java
@@ -0,0 +1,16 @@
+package Java1;
+
+class Main {
+    public void DFS(int n) {
+        if (n == 0) return;
+        else {
+            DFS(n-1);
+            System.out.println(n + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.DFS(3);
+    }
+}
