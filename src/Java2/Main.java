745a3d070bfb64b4bdce9ba4fff26c51d84c7d34
sujung00 <rlatnwjdirin@naver.com>
2022-10-01 22:54:02 +0900

    이진수 출력(재귀)

---
+++ src/Java2/Main.java
@@ -0,0 +1,16 @@
+package Java2;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.DFS(11);
+    }
+
+    public void DFS(int n) {
+        if (n == 0) return;
+        else {
+            DFS(n/2);
+            System.out.print(n%2 + " ");
+        }
+    }
+}
