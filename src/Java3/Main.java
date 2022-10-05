7d09a35d13e4a7ab7ccaa32a282628c406f5fec3
sujung00 <rlatnwjdirin@naver.com>
2022-10-05 23:27:14 +0900

    팩토리얼

---
+++ src/Java3/Main.java
@@ -0,0 +1,13 @@
+package Java3;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        System.out.println(T.DFS(5));
+    }
+
+    public int DFS(int n) {
+        if (n == 1) return 1;
+        else return n*DFS(n-1);
+    }
+}
