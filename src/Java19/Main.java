c2953bd4357ff334108d6c50dacbec63c8fec6aa
sujung00 <rlatnwjdirin@naver.com>
2022-11-18 20:28:01 +0900

    순열 구하기

---
+++ src/Java19/Main.java
@@ -0,0 +1,35 @@
+package Java19;
+
+import java.util.Scanner;
+
+class Main {
+    static int[] pm, ch, arr;
+    static int n, m;
+    public void DFS(int L) {
+        if (L==m) {
+            for (int x : pm) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = 0; i < n; i++){
+                if (ch[i]==0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        ch = new int[n];
+        pm = new int[m];
+        T.DFS(0);
+    }
+}
