4735fcd8081322dc17001689fce2809cfffaffeb
sujung00 <rlatnwjdirin@naver.com>
2022-11-20 23:11:27 +0900

    수열 추측하기

---
+++ src/Java21/Main.java
@@ -0,0 +1,47 @@
+package Java21;
+
+import java.util.Scanner;
+
+class Main {
+    static int[] b, p, ch;
+    static int n, f;
+    boolean flag = false;
+    int[][] dy = new int[35][35];
+    public int combi(int n, int r) {
+        if (dy[n][r]>0) return dy[n][r];
+        if (n==r || r==0) return 1;
+        else return dy[n][r]=combi(n-1, r-1)+combi(n-1,r);
+    }
+
+    public void DFS(int L, int sum) {
+        if (L==n){
+            if (sum==f){
+                for (int x : p) System.out.print(x + " ");
+                flag = true;
+            }
+        } else {
+            for (int i = 0; i<=0; i++) {
+                if (ch[i]==0) {
+                    ch[i]=1;
+                    p[L]=i;
+                    DFS(L+1, sum+(p[L]*b[L]));
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        f = scanner.nextInt();
+        b = new int[n];
+        p = new int[n];
+        ch = new int[n+1];
+        for (int i = 0; i < n; i++){
+            b[i] = T.combi(n-1, 1);
+        }
+        T.DFS(0, 0);
+    }
+}
